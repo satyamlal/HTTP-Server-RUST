ac7d52df78057c789b8c1e2b3aa1455a9777480a
Satyam Lal <ssatyamlal@gmail.com>
2026-01-09 16:31:11 +0530

    feat: add echo server functionality

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ fn main() {
     for stream in listener.incoming(){
         match stream {
             Ok(stream) => {
-                let _stream = stream.unwrap();
-                eprintln!("Connection established!");
+                handle_client(stream);
             }
             Err(e) => {
                 eprintln!("Failed to establish a socket connection!");
             }
         }
+    }
+}
+
+fn handle_client(mut stream:TcpStream){
+    let mut buf = [0;512];
+    loop {
+        let bytes_read = stream.read(&mut buf).expect("Failed to read the incoming connection!");
+        if bytes_read == 0 {
+            return;
+        }
+        stream.write_all(&buf[0..bytes_read]).expect("Failed to write to client");
     }
 }
