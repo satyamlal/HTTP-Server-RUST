5e0cfdcb220eb66ee3559c3f2e8b48014456b5be
Satyam Lal <ssatyamlal@gmail.com>
2026-02-03 01:19:59 +0530

    feat: extract URL path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::{TcpListener, TcpStream};
+use std::io::{BufReader, BufRead, Write, Read};
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Could not bind");
     for stream in listener.incoming(){
@@ -14,12 +16,36 @@ fn main() {
 }
 
 fn handle_client(mut stream:TcpStream){
-    let mut buf = [0;512];
-    loop {
-        let bytes_read = stream.read(&mut buf).expect("Failed to read the incoming connection!");
-        if bytes_read == 0 {
-            return;
-        }
-        stream.write_all(&buf[0..bytes_read]).expect("Failed to write to client");
+    // feat: Buffering and Security to prevent DOS attacks
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut handle = buf_reader.by_ref().take(2048);
+    let mut request_line = String::new();
+
+    if let Err(e) = handle.read_line(&mut request_line) {
+        println!("Failed to read line: {}", e);
+        return;
+    }
+
+    // Checking for malicious cutoff or empty request
+    if request_line.is_empty(){
+        return;
+    }
+
+    let parts:Vec<&str> = request_line.trim().split_whitespace().collect();
+
+    if parts.len() < 2 {
+        println!("Invalid Request Format!");
+        continue;
+    }
+    let path = parts[1];
+
+    let response = if path == "/" {
+        b"HTTP/1.1 200 OK\r\n\r\n";
+    } else {
+        b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    }
+    
+    if let Err(e) = buf_reader.get_mut().write_all(response) {
+        println!("Failed to write response: {}", e);
     }
 }
