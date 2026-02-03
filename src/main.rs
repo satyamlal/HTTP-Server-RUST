7f188193e9e918f3846d4137858a61eb4f4a471c
Satyam Lal <ssatyamlal@gmail.com>
2026-02-03 13:26:21 +0530

    feat: read /echo/ + /user-agent headers

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 fn handle_client(mut stream:TcpStream){
     // feat: Buffering and Security to prevent DOS attacks
     let mut buf_reader = BufReader::new(&mut stream);
-    let mut handle = buf_reader.by_ref().take(2048); // Added .take(2048) to prevent crashing via RAM OOM issue.
+    let mut handle = buf_reader.by_ref().take(2048); // .take(2048) to prevent crashing via RAM OOM issue.
     let mut request_line = String::new();
 
     if let Err(e) = handle.read_line(&mut request_line) {
@@ -41,13 +41,27 @@ fn handle_client(mut stream:TcpStream){
     }
     let path = parts[1];
 
+    // Reading header for /echo/ and /user-agent
     let response = if path == "/" {
-        b"HTTP/1.1 200 OK\r\n\r\n";
+        b"HTTP/1.1 200 OK\r\n\r\n".to_string();
+    } else if path == "/echo/" {
+        let echo_str = path.strip_prefix("/echo/").unwrap();
+        let echo_len = echo_str.len();
+        format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n{}", echo_len, echo_str);
+    } else if path == "/user-agent" {
+        let mut user_agent_value = String::new();
+        for line in buf_reader.lines(){
+            break;
+        }
+        if line.starts_with("User-Agent: "){
+            user_agent_value = line[12..].trim().to_string();
+        }
+        format!("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n{}", user_agent_value.len(), user_agent_value);
     } else {
-        b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        b"HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string();
     }
     
-    if let Err(e) = buf_reader.get_mut().write_all(response) {
+    if let Err(e) = buf_reader.get_mut().write_all(response.as_bytes()) {
         println!("Failed to write response: {}", e);
     }
 }
