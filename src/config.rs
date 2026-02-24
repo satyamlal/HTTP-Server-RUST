c7325e71391787aa935681bc0514aa223848dac6
Satyam Lal <ssatyamlal@gmail.com>
2026-02-24 20:07:42 +0530

    fix bugs

--- src/config.rs
+++ src/config.rs
@@ -1,22 +1,21 @@
 use std::env;
 
 pub struct Config {
-    pub directory: String,
+    pub directory: String, 
 }
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
+        args.next(); 
         let mut directory = String::from(".");
-
-        while let Some(args) = args.next() {
-            if args == "--directory" {
+        while let Some(arg) = args.next() { 
+            if arg == "--directory" {
                 directory = match args.next() {
-                    Some(val) = val,
+                    Some(val) => val,
                     None => return Err("No value provided for --directory"),
                 };
             }
         }
-        Ok(config {directory} )
+        Ok(Config { directory }) 
     }
 }
