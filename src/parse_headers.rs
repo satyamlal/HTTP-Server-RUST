6d5ba01d518e9d8eb701c14f2e3dc7ce385799d4
Satyam Lal <ssatyamlal@gmail.com>
2026-02-27 13:38:09 +0530

    Fix: DDOS vulnerability, Rigid Type signature, overwriting duplicate headers

--- src/parse_headers.rs
+++ src/parse_headers.rs
@@ -1,26 +1,48 @@
 use std::{
     collections::HashMap,
-    io::{BufRead, BufReader},
-    net::TcpStream,
+    io::{BufRead, BufReader, ErrorKind, Read},
 };
 
-pub fn parse_headers(buf_reader: &mut BufReader<&mut TcpStream>) -> HashMap<String, String> {
+// to prevet DDOS attacks
+const MAX_HEADER_LINE_LEN: u64 = 8192; //8KB max per line
+const MAX_HEADERS: usize = 100; // Limiting total headers parsed
+
+pub fn parse_headers(buf_reader: &mut impl BufRead) -> Result<HashMap<String, String>, Error> {
     let mut headers = HashMap::new();
 
-    loop {
+    for _ in 0..MAX_HEADERS {
         let mut line = String::new();
-        match buf_reader.read_line(&mut line) {
-            Ok(0) => break,
-            Ok(_) => {
-                if line == "\r\n" || line == "\n" {
-                    break;
-                }
-                if let Some((key, value)) = line.split_once(":") {
-                    headers.insert(key.trim().to_lowercase(), value.trim().to_string());
-                }
-            }
-            Err(_) => break,
+
+        let bytes_read = buf_readers.by_ref().take(MAX_HEADER_LINE_LEN).read_line(&mut line)?; // Guard against OOM
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        if bytes_read as u64 == MAX_HEADER_LINE_LEN && !line.ends_with("\n") {
+            return Err(Error::new(ErrorKind::InvalidData, "Header line exceeded maximum length!"));
+        }
+
+        let trimmed = line.trim();
+
+        if trimmed.is_empty() {
+            return Ok(headers);
+        }
+
+        if let Some((key, value)) = line.split_once(":") {
+            let header_key = key.trim().to_lowercase();
+            let header_value = value.trim().to_string();
+
+            // handling duplicate headers
+            headers.entry(header_key)
+                .and_modify(|existing_val| {
+                    existing_val.push_str(", ");
+                    existing_val.push_str(&header_value);
+                })
+                .or_insert(header_value);
+        } else {
+            return Err(Error::new(ErrorKind::InvalidData, "Malformed HTTP Header"));
         }
     }
-    headers
+    Err(Error::new(ErrorKind::InvalidData, "Too Many headers in request!"));
 }
