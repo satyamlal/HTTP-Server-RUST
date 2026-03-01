226936d906119d0324bff8a21bf8dbbc33902f16
Satyam Lal <ssatyamlal@gmail.com>
2026-03-01 21:38:31 +0530

    Pre-allocate capacity to avoid excessive reallocations

--- src/compress_data.rs
+++ src/compress_data.rs
@@ -1,8 +1,10 @@
 use flate2::{write::GzEncoder, Compression};
-use std::io::Write;
+use std::io::{self, Write};
 
-pub fn compress_data(data: &str) -> Vec<u8> {
-    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
-    encoder.write_all(data.as_bytes()).unwrap();
-    encoder.finish().unwrap()
+pub fn compress_data(data: &[u8]) -> io::Result<Vec<u8>> {
+    let capacity = std::cmp::max(data.len() / 3, 64);
+    let mut encoder = GzEncoder::new(Vec::with_capacity(capacity), Compression::default());
+    
+    encoder.write_all(data)?;
+    encoder.finish()
 }
